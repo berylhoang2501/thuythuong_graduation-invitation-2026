8d91595f5c0b38a613284d9734bf20156121e63c
Hoang Ngoc Thuy Thuong <152646327+berylhoang2501@users.noreply.github.com>
2026-07-19 01:26:04 +0700

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,7 @@
 module.exports = {
   pathPrefix: "/thuythuong_graduation-invitation-2026",
+  siteMetadata: {
+    title: "Lễ Tốt Nghiệp 2026 - Hoàng Ngọc Thủy Thương",
+  },
   plugins: ["gatsby-plugin-react-helmet"],
 };
