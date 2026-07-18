72cf3cd2bebea8d44525747b3d73c95180bb0343
Hoang Ngoc Thuy Thuong <152646327+berylhoang2501@users.noreply.github.com>
2026-07-19 00:59:05 +0700

    Delete utils directory

--- utils/getQueryValue.js
+++
@@ -1,5 +0,0 @@
-export default function getQueryValue(location, name) {
-  if (!location || !location.search) return '';
-  const params = new URLSearchParams(location.search);
-  return params.get(name) || '';
-}
